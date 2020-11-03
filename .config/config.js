50eea1efd628e21cdaf64c7dff08e5ad1148faf9
Hakkı Sağdıç <hakkisagdic@gmail.com>
2020-11-03 16:05:09 +0300

    Create config.js

---
+++ .config/config.js
@@ -0,0 +1,27 @@
+{
+    "accounts": [
+        {
+            "email": "example@gmail.com",
+            "password": "abc123",
+            "totp": "EMNCF83ULU39CYFOPAQW8VHZBC7S7CTWKDXM19C2S2JYI69R39NE"
+        },
+    ],
+    "onlyWeekly": false,
+    "runOnStartup": true,
+    "cronSchedule": "0 12 * * *",
+    "runOnce": false,
+    "logLevel": "info",
+    "baseUrl": "https://example.com",
+    "email": {
+        "smtpHost": "smtp.gmail.com",
+        "smtpPort": 587,
+        "emailSenderAddress": "hello@gmail.com",
+        "emailSenderName": "Epic Games Captchas",
+        "emailRecipientAddress": "hello@gmail.com",
+        "secure": false,
+        "auth": {
+            "user": "hello@gmail.com",
+            "pass": "abc123",
+        },
+    }
+}
